b3c8cfc06eb79a3ff911d1008acac6cca9ee93da
Vladimir Schneider <vladimir@vladsch.com>
2018-05-13 14:38:37 -0400

    update tests

--- test/type.test.js
+++ test/type.test.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const each = require('jest-each');
+const jestEach = require('jest-each');
 const utilTypes = require('util-type-funcs');
 const isString = utilTypes.isString;
 const isFunction = utilTypes.isFunction;
@@ -22,11 +22,74 @@ const toArrayIndex = utilTypes.toArrayIndex;
 
 // TODO: add tests
 
-each([
+jestEach([
+    [undefined, 'undefined', false],
+    [null, 'null', false],
+    [NaN, 'NaN', false],
+    ["test", '"test"', false],
+    [true, 'true', false],
+    [false, 'false', false],
+    [0, '0', true],
+    [5, '5', true],
+    [-5, '-5', true],
+    [-5.3, '-5', true],
+    ['0', '"0"', true],
+    ['5', '"5"', true],
+    ['5.3', '"5"', true],
+    ['-1', '"-1"', true],
+    ['-5', '"-5"', true],
+    ['-5.3', '"-5.3"', true],
 ])
-    .describe(`arg: '%s, param: '%s'`, (arg, param, expected) => {
-        test(`'${arg}'.suffixWith('${param}') === '${expected}'`, () => {
-            expect(suffixWith(arg, param)).toBe(expected);
+    .describe('isNumeric', (value, valueText, expectedVal) => {
+        test(`isNumeric(${valueText}) === ${expectedVal}`, () => {
+            expect(isNumeric(value)).toBe(expectedVal);
         });
     });
 
+jestEach([
+    [undefined, 'undefined', undefined],
+    [null, 'null', null],
+    [NaN, 'NaN', NaN],
+    ["test", '"test"', "test"],
+    [true, 'true', true],
+    [false, 'false', false],
+    [0, '0', 0],
+    [5, '5', 5],
+    [-5, '-5', -5],
+    [-5.3, '-5', -5.3],
+    ['0', '"0"', 0],
+    ['5', '"5"', 5],
+    ['5.3', '"5"', 5.3],
+    ['-1', '"-1"', -1],
+    ['-5', '"-5"', -5],
+    ['-5.3', '"-5.3"', -5.3],
+])
+    .describe('toNumber', (value, valueText, expectedVal) => {
+        test(`toNumber(${valueText}) === ${expectedVal}`, () => {
+            expect(toNumber(value)).toBe(expectedVal);
+        });
+    });
+
+jestEach([
+    [undefined, 'undefined', undefined],
+    [null, 'null', null],
+    [NaN, 'NaN', NaN],
+    ["test", '"test"', "test"],
+    [true, 'true', true],
+    [false, 'false', false],
+    [0, '0', 0],
+    [5, '5', 5],
+    [-5, '-5', -5],
+    [-5.3, '-5', -5.3],
+    ['0', '"0"', 0],
+    ['5', '"5"', 5],
+    ['5.3', '"5"', 5.3],
+    ['-1', '"-1"', -1],
+    ['-5', '"-5"', -5],
+    ['-5.3', '"-5.3"', -5.3],
+])
+    .describe('toNumber', (value, valueText, expectedVal) => {
+        test(`toNumber(${valueText}) === ${expectedVal}`, () => {
+            expect(toNumber(value)).toBe(expectedVal);
+        });
+    });
